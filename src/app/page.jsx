91dc4aba0c94f7727978ded73cd64e852c1c66b2
rangwanisailesh <s.rangwani44@gmail.com>
2025-02-24 23:59:42 +0530

    create wallet, wallet update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,13 @@
 import { HomeComp } from "@/components/home";
 
+export const metadata = {
+  title : 'SRT Token',
+  description: 'Claim free SRT Token after every 6 hours.',
+  icons: {
+    icon: '/logo.png'
+  }
+}
+
 export default function Home() {
   return (
    <div>
--- src/components/home.js
+++ src/components/home.js
@@ -17,6 +17,10 @@ import {
 import { GiBearFace } from "react-icons/gi";
 import { FaEthereum } from "react-icons/fa";
 import { PiWalletFill } from "react-icons/pi";
+import { FaRegClipboard } from "react-icons/fa";
+import { IoMdExit } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import { IoReload } from "react-icons/io5";
 
 export const HomeComp = () => {
 
@@ -25,7 +29,8 @@ export const HomeComp = () => {
     const [balances, setBalances] = useState({ srt: '', eth: '', });
     const [inputs, setInputs] = useState({ email: '', pvtkey: '' });
     const [account, setAccount] = useState('');
-    const [network, setNetwork] = useState(false);
+    const [pvtKey, setPvtKey] = useState('');
+    const [userbox, setUserbox] = useState(false);
 
     const importwallet = async (e) => {
 
@@ -49,6 +54,8 @@ export const HomeComp = () => {
             }
 
             const acc = web3.eth.accounts.privateKeyToAccount(pvtKey);
+
+            localStorage.setItem("SRT Token", acc.address);
 
             setAccount(acc.address);
 
@@ -75,13 +82,85 @@ export const HomeComp = () => {
 
     };
 
-    const copytoclipboard = () => {
-        navigator.clipboard.writeText(account);
-        setMsg({ show: true, type: 'success', message: 'Wallet Address Copied' });
+    const createWallet = async () => {
+
+        const wallet = web3.eth.accounts.create();
+
+        if (wallet.address) {
+            setAccount(wallet.address);
+            setPvtKey(wallet.privateKey);
+            setModal({ show: true, type: 'create' })
+            localStorage.setItem("SRT Token", wallet.address);
+            const balanceWei = await web3.eth.getBalance(wallet.address);
+            const balanceEth = parseFloat(web3.utils.fromWei(balanceWei, "ether")).toFixed(4);
+            const tokenBal = await tokenContract.methods.balanceOf(wallet.address).call();
+            const formattedBalance = web3.utils.fromWei(tokenBal, "ether");
+
+            setBalances({ eth: balanceEth, srt: formattedBalance });
+
+            setMsg({ show: true, type: 'success', message: 'Wallet Connected Successfully.' });
+            setTimeout(() => {
+                setMsg({ show: false, type: '', message: '' });
+            }, 3500);
+        }
+    };
+
+    const copytoclipboard = (add) => {
+        navigator.clipboard.writeText(add);
+        setMsg({ show: true, type: 'success', message: 'Copied' });
         setTimeout(() => {
             setMsg({ show: false, type: '', message: '' });
-        }, 3500);  
+        }, 3500);
     };
+
+    const refresh = async () => {
+        setMsg({ show: true, type: 'info', message: 'Updating Balances...' });
+        const balanceWei = await web3.eth.getBalance(account);
+        const balanceEth = parseFloat(web3.utils.fromWei(balanceWei, "ether")).toFixed(4);
+        const tokenBal = await tokenContract.methods.balanceOf(account).call();
+        const formattedBalance = web3.utils.fromWei(tokenBal, "ether");
+
+        setBalances({ eth: balanceEth, srt: formattedBalance });
+        setModal({ show: false, type: '' });
+
+        setMsg({ show: true, type: 'success', message: 'Balances Updated Successfully.' });
+        setTimeout(() => {
+            setMsg({ show: false, type: '', message: '' });
+        }, 3500);
+    };
+
+    useEffect(() => {
+
+        const token = localStorage.getItem('SRT Token');
+
+        const connectwallet = async () => {
+
+            try {
+                setAccount(token);
+
+                const balanceWei = await web3.eth.getBalance(token);
+                const balanceEth = parseFloat(web3.utils.fromWei(balanceWei, "ether")).toFixed(4);
+                const tokenBal = await tokenContract.methods.balanceOf(token).call();
+                const formattedBalance = web3.utils.fromWei(tokenBal, "ether");
+
+                setBalances({ eth: balanceEth, srt: formattedBalance });
+                setModal({ show: false, type: '' });
+
+                setMsg({ show: true, type: 'success', message: 'Wallet Connected Successfully.' });
+                setTimeout(() => {
+                    setMsg({ show: false, type: '', message: '' });
+                }, 3500);
+            } catch (err) {
+                localStorage.removeItem('SRT Token');
+            }
+
+        };
+
+        if (token) {
+            connectwallet();
+        }
+
+    }, []);
 
     return (
         <div className="bg-[#0a254d] w-full h-full min-h-[100vh] relative">
@@ -103,17 +182,42 @@ export const HomeComp = () => {
                         </div>
                     </div>
 
-                        <div className="flex justify-end w-full duration-300">
-                            <button onClick={() => copytoclipboard()} className="flex items-center my-auto space-x-2 px-3 py-2 text-blue-700 rounded-full border-2 border-blue-400 bg-gradient-to-r from-sky-100 to-sky-300 hover:from-sky-300 hover:to-sky-100 duration-300">
-                                <PiWalletFill className="text-xl" />
+                    <div className="flex justify-end w-full duration-300 relative">
+                        <button onClick={() => refresh()} className="h-10 w-10 rounded-full border-2 border-blue-400 bg-gradient-to-r from-sky-100 to-sky-300 hover:scale-[105%] duration-300 text-blue-700 mr-2">
+                            <span className="flex justify-center items-center m-auto h-full">
+                                <IoReload />
+                            </span>
+                        </button>
+
+                        <button onClick={() => setUserbox(!userbox)} className="h-10 w-10 rounded-full border-2 border-blue-400 bg-gradient-to-r from-sky-100 to-sky-300 hover:scale-[105%] duration-300 text-blue-700">
+                            <span className="flex justify-center items-center m-auto h-full">
+                                <FaUser />
+                            </span>
+                        </button>
+
+                        <div className={`${userbox && account ? 'absolute top-12 right-0 bg-white shadow-lg p-5 rounded-lg duration-300 z-40' : 'hidden duration-300'}`}>
+                            <button onClick={() => {copytoclipboard(account); setUserbox(false);}} className="flex items-center my-auto space-x-2 text-black border-b pb-2 mb-2 hover:scale-[105%] duration-300">
+                                <PiWalletFill className="text-xl text-blue-600" />
                                 <span className="inline">{account.slice(0, 6)}...{account.slice(-4)}</span>
                             </button>
+                            <button onClick={() => {
+                                setBalances({ srt: '', eth: '', });
+                                setAccount('');
+                                setModal({ show: true, type: '' });
+                                setPvtKey('');
+                                setInputs({ email: '', pvtkey: '' });
+                                setUserbox(false);
+                            }} className="flex items-center my-auto space-x-2 text-black hover:scale-[105%] duration-300">
+                                <IoMdExit className="text-xl text-blue-600" />
+                                <span className="inline">Exit</span>
+                            </button>
                         </div>
+                    </div>
 
                 </div>
 
                 {/* Balances */}
-                <div className="grid grid-cols-3 gap-6">
+                <div className="lg:flex items-center my-auto grid md:grid-cols-2 grid-cols-1 gap-6">
 
                     <div className="p-5 rounded-lg border-2 border-blue-400 w-full bg-white shadow-lg flex space-x-4 relative overflow-hidden">
 
@@ -121,19 +225,19 @@ export const HomeComp = () => {
                             <Skeleton variant="rounded" width="100%" height="100%" animation="wave" />
                         </div>
 
-                        <div className="h-20 w-20 rounded-full bg-[#0a254d] drop-shadow-lg">
+                        <div className="h-16 w-16 md:h-20 md:w-20 rounded-full bg-[#0a254d] drop-shadow-lg">
                             <span className="flex justify-center items-center m-auto h-full">
-                                <FaEthereum className="text-white h-10 w-10" />
+                                <FaEthereum className="text-white h-8 w-8 md:h-10 md:w-10" />
                             </span>
                         </div>
 
                         <div className={`${poppins_regular} space-y-2`}>
 
-                            <div className={`text-xl drop-shadow-lg text-[#0a254d]`}>
-                                ETH Balance
+                            <div className={`md:text-xl drop-shadow-lg text-[#0a254d]`}>
+                               Sepolia ETH Balance
                             </div>
 
-                            <div className="text-gray-700 font-semibold text-lg">
+                            <div className="text-gray-700 font-semibold md:text-lg">
                                 {balances.eth}
                             </div>
 
@@ -147,19 +251,19 @@ export const HomeComp = () => {
                             <Skeleton variant="rounded" width="100%" height="100%" animation="wave" />
                         </div>
 
-                        <div className="h-20 w-20 rounded-full bg-[#0a254d] drop-shadow-lg">
+                        <div className="h-16 w-16 md:h-20 md:w-20 rounded-full bg-[#0a254d] drop-shadow-lg">
                             <span className="flex justify-center items-center m-auto h-full">
-                                <GiBearFace className="text-white h-10 w-10" />
+                                <GiBearFace className="text-white h-8 w-8 md:h-10 md:w-10" />
                             </span>
                         </div>
 
                         <div className={`${poppins_regular} space-y-2`}>
 
-                            <div className={`text-xl drop-shadow-lg text-[#0a254d]`}>
+                            <div className={`md:text-xl drop-shadow-lg text-[#0a254d]`}>
                                 SRT Balance
                             </div>
 
-                            <div className="text-gray-700 font-semibold text-lg">
+                            <div className="text-gray-700 font-semibold md:text-lg">
                                 {balances.srt}
                             </div>
 
@@ -170,13 +274,32 @@ export const HomeComp = () => {
 
             </div>
 
+            {/* Modal */}
             <div className={`${poppins_regular} ${modal.show ? 'fixed h-[100vh] w-full bg-[#00000094] top-0 left-0 z-50' : 'hidden'}`}>
 
                 <div className="absolute top-1/2 left-1/2 bg-white rounded-lg shadow-lg p-3 lg:p-5 focus:outline-none -translate-x-1/2 -translate-y-1/2 w-[90%] md:w-[65%] lg:min-w-[20%] lg:w-auto md:max-w-[80%]">
 
                     {modal.type == 'create' ?
                         <div>
-                            create
+                            <div className={`${barlow} text-2xl font-semibold flex justify-center mx-auto text-center base-text border-b pb-4 mb-4`}>
+                                Wallet Credentials
+                            </div>
+
+                            <div className="text-center flex justify-center mx-auto">
+                                Please copy your new wallet Credentials.
+                            </div>
+
+                            <div className="my-2">
+                                <span className="font-semibold">Address : </span><span className="inline">{account.slice(0, 6)}...{account.slice(-4)}</span> <button onClick={() => copytoclipboard(account)} className="text-blue-500 hover:text-blue-800"><FaRegClipboard /></button>
+                            </div>
+
+                            <div className="my-2">
+                                <span className="font-semibold">Private Key : </span><span className="inline">{pvtKey.slice(0, 6)}...{pvtKey.slice(-4)}</span> <button onClick={() => copytoclipboard(pvtKey)} className="text-blue-500 hover:text-blue-800"><FaRegClipboard /></button>
+                            </div>
+
+                            <button type="button" onClick={() => setModal({ show: false, type: '' })} className="flex justify-center mx-auto btn1">
+                                Close
+                            </button>
                         </div> :
                         modal.type == 'import' ?
                             <div>
@@ -211,7 +334,7 @@ export const HomeComp = () => {
                                 </div>
                                 <div className="grid grid-cols-1 gap-4">
 
-                                    <button onClick={() => setModal({ ...modal, type: 'create' })} className="btn1 w-fit flex justify-center mx-auto">Create Wallet</button>
+                                    <button onClick={() => createWallet()} className="btn1 w-fit flex justify-center mx-auto">Create Wallet</button>
                                     <button onClick={() => setModal({ ...modal, type: 'import' })} className="btn1 w-fit flex justify-center mx-auto">Import Wallet</button>
 
                                 </div>
